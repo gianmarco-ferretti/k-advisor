60d92fee7a53a9b19275e2f6747db97ed6ec0eda
ValerioPacciani <valerio.pacciani@gmail.com>
2021-06-22 12:17:28 +0200

    commitVale

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from "react-redux";
 
 //const store = createStore()
-
+//Ciao GM
 ReactDOM.render(
     <React.StrictMode>
         <Provider>
