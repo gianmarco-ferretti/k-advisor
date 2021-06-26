308f0edb8fc63ff755ef98cb8e3a4aae75cbad6c
FabrizioCapoccia <fabrizio997@gmail.com>
2021-06-26 17:36:37 +0200

    Fab

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -2,24 +2,39 @@
 import React from 'react'
 import { Row, Card, Button, Col } from 'react-bootstrap'
 
-export default function MyCard() {
+export default function card() {
     return (
             <Card>
                 <Row>
-                    <Col className="col-4">
-                        <Card.Img id="img" src="https://s3-alpha-sig.figma.com/img/e6fe/d0a6/b5e1065dae506702ff89b0b602c1ca9d?Expires=1625443200&Signature=cyKakjPpgAmiH7EzxSqpFm4IQlE~SSFV8uCjICXs5slBNrX~XnXAuaoBZdPXT1SPe5X4FTeDvQ0MUCJlUKWkeVBru9NUxP0Kcoxfy9TL~uN2lExRp~VhIWPvaJ0tebfv5DJPpuot08ncvPbZpbbODWQs8ChRS0W1OsEP1i0FJ-C-LuNB3sBaJP7S84VpRKk00OPQzHzz67u3Ze2nd~OdvrWAAquPAIZT1Eih6wQn3yV7Hh3AkZACOVz19qAGCcEAk-9oPzY2Feo-x9xsHHKDKyWz4tL0LrA-CgfMYSMXaZn6EumxpUDojhbmhHy9EGG-uLWoAftFpjaeIte0g5rY3A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
+                    <Col className="col-2 p-0">
+                        <Card.Img id="img"/>
                     </Col>
-                    <Col>
-                        <Card.Body className="col-8">
-                            <Card.Title>A tutta pizza Frosinone</Card.Title>
-                            <Card.Text>
-                                <div id="bubble"></div>
-                                191 recensioni
+                    <Col className="col-10">
+                        <Card.Body className="p-0">
+                            <Card.Title className="text-start">A tutta pizza Frosinone</Card.Title>
+                            <Card.Text className="d-flex mb-0">
+                                <div className="d-flex mt-1">
+                                    <a className="bubble"></a>
+                                    <a className="bubble"></a>
+                                    <a className="bubble"></a>
+                                    <a className="bubble"></a>
+                                    <a className="bubble"></a>
+                                </div>
+                                <a className="d-flex ms-1">191 recensioni</a>
+                                <p className="d-flex ms-1">|</p>
+                                <p className="d-flex ms-1 text-danger">Oggi chiuso</p>
                             </Card.Text>
-                            <Card.Footer>
-                                "Pizza top"
+                            <Card.Text className="d-flex">
+                                <p>Pizza italiana</p>
+                                <p className="ms-1">|</p>
+                                <p className="ms-1">€</p>
+                                <p className="ms-1">|</p>
+                                <a className="ms-1">Menù</a>
+                            </Card.Text>
+                            <Card.Footer className="d-flex flex-column align-items-start">
+                                <p>"Pizza top"</p>
+                                <Button variant="warning" className="rounded-pill">Prenota</Button>
                             </Card.Footer>
-                            <Button variant="warning">Prenota</Button>
                         </Card.Body>
                     </Col>
                 </Row>
